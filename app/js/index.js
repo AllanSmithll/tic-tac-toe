fc3e2b333e4ce418df2c02b6232149238dbbfea3
allansmithll <allanammancio2004@gmail.com>
2023-11-01 21:39:10 -0300

    Novas adições úteis

--- app/js/index.js
+++
@@ -1,7 +0,0 @@
-const nameProject = "Tic Tac Toe";
-window.onload = () => {
-    let elements = document.querySelectorAll('.nameProject');
-    elements.forEach(element => {
-        element.textContent = nameProject;
-    });
-};
